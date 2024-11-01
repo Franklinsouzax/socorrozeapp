9c1fc5ae8325ecdf56818758754fb2f023cc9e6f
Franklinsouzax <Franklinsouzax@gmail.com>
2024-11-01 16:58:05 -0300

    Minhas alterações antes de puxar as atualizações

--- App.js
+++ App.js
@@ -1,30 +1,47 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
+import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Welcome from './src/screens/Welcome';
 import Login from './src/screens/Login';
 import Cadastro from './src/screens/Cadastro';
 import Home from './src/screens/Home';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      <StatusBar style="auto" />
+      <Tab.Navigator
         initialRouteName='Welcome'
-      >
-        <Stack.Screen 
-          name='Welcome'
-          component={Welcome}
-        />
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-        
-      </Stack.Navigator>
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Welcome') {
+              iconName = focused ? 'information-circle' : 'information-circle-outline';
+            } else if (route.name === 'Cadastro') {
+              iconName = focused ? 'person-add' : 'person-add-outline';
+            } else if (route.name === 'Login') {
+              iconName = focused ? 'log-in' : 'log-in-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Welcome' component={Welcome} />
+        <Tab.Screen name='Cadastro' component={Cadastro} />
+        <Tab.Screen name='Login' component={Login} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ src/screens/Cadastro.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet,Text,View} from "react-native";
+
+export default function Cadastro(){
+    return(
+        <View>
+
+
+            
+        </View>
+    )
+}
+
+const Styles = StyleSheet.create({
+
+})
